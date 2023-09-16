1bcd673c52488c840f2ff31ed75a5196b137a534
1amSumit <sj779619@gmail.com>
2023-09-16 12:35:14 +0530

    images added and Expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.yellow,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Expanded(child: Image.asset('images/dice1.png')),
+        Expanded(child: Image.asset('images/dice2.png')),
+      ],
+    );
   }
 }
