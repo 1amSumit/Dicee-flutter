64c3e4249bf95499e3f7f2dad2512bd103a0f3dd
1amSumit <sj779619@gmail.com>
2023-09-17 06:00:42 +0530

    changing the state using setState and used function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() {
   return runApp(
@@ -27,6 +28,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  getDiceNumber(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,11 +43,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextButton(
-              onPressed: (){
-                setState(() {
-                  leftDiceNumber = 3;
-                });
-              },
+              onPressed: getDiceNumber,
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
@@ -47,11 +51,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextButton(
-              onPressed: (){
-                setState(() {
-                  rightDiceNumber = 2;
-                });
-              },
+              onPressed: getDiceNumber,
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
